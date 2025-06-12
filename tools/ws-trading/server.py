e834fcb438434a8b2491108e70b434f773c93d37
rohman35819 <rohman35819@email.com>
2025-06-13 02:11:35 +0700

    Update otomatis

--- tools/ws-trading/server.py
+++ tools/ws-trading/server.py
@@ -15,7 +15,8 @@ async def price_feed(websocket, path):
         await websocket.send(data)
         await asyncio.sleep(1)
 
-start_server = websockets.serve(price_feed, "localhost", 8765)
+start_server = websockets.serve(price_feed, "0.0.0.0", 8765)
+
 
 print("WebSocket server running on ws://localhost:8765")
 asyncio.get_event_loop().run_until_complete(start_server)
